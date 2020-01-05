a0745fe8902b615b7e048b0636b57a48402354e4
oToToT <ty1208chiang@gmail.com>
2020-01-05 19:42:48 +0800

    fix include path

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$conf = json_decode(file_get_contents("config.json"), true);
+$conf = json_decode(file_get_contents(__DIR__.'/config.json'), true);
 define(JUDGEGIRL_URL, $conf['JUDGEGIRL_PATH']);
 define(URL_BASE, $conf['URL_BASE']);
 define(TMP_PATH, $conf['TMP_PATH']);
